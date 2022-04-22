767ea2bdc9bd2aece93d381d8c736f5c6193f640
swegmonet <j00seif00d0@gmail.com>
2022-04-22 15:37:10 -0700

    add all

--- combos.py
+++ combos.py
@@ -1,61 +1,98 @@
 import csv
+from functools import cmp_to_key
 
 LETTER_VALUES = {
-  'a': 0,
+  'd': 0,
   'e': 0,
   'i': 0,
-  'o': 0,
-  'u': 0,
-  't': 1,
-  'n': 1,
-  's': 1,
-  'r': 2,
-  'd': 2,
-  'l': 2,
-  'c': 3,
-  'm': 3,
-  'f': 3,
-  'w': 3,
-  'y': 3,
-  'g': 4,
-  'p': 4,
-  'b': 4,
-  'v': 4,
-  'k': 4,
+  'n': 0,
+  'r': 0,
+  's': 0,
+  't': 0,
+  'a': 1,
+  'c': 1,
+  'f': 1,
+  'l': 1,
+  'm': 1,
+  'o': 1,
+  'w': 1,
+  'y': 1,
+  'b': 2,
+  'g': 2,
+  'k': 2,
+  'p': 2,
+  'u': 2,
+  'h': 3,
+  'v': 3,
+  'j': 4,
+  'z': 4,
+  'x': 5,
+  'q': 6
 }
 
 def determine_combo_value(combo_row):
   combo_value = 0
   for word in combo_row:
-    for letter in word:
-      if letter in ', ':
-        continue
-      combo_value += LETTER_VALUES.get(letter, 5)
-
+    combo_value += determine_word_value(word)
   return combo_value
 
+def determine_word_value(word):
+  word_value = 0
+  for letter in word:
+    if letter in ', ':
+      continue
+    word_value += LETTER_VALUES.get(letter, 6)
+  return word_value
+
+def count_values_under_amount(amount, value_list):
+  count = 0
+  for value in value_list:
+    if value < amount:
+      count += 1
+  return count
+
+def compare_word_values(item1, item2):
+  if determine_word_value(item1) < determine_word_value(item2):
+    return -1
+  elif determine_word_value(item1) > determine_word_value(item2):
+    return 1
+  else:
+    return 0
 
 def print_best_combos():
   value_to_combos_dict = {}
-  csv_file = open("combos.csv", "r")
+  csv_file = open("sortedcombos.csv", "r")
+  write_csv_file = open('bestvaluecombos.csv', 'w')
   reader = csv.reader(csv_file, delimiter=',', quotechar='|')
   for row in reader:
     row_value = determine_combo_value(row)
-    if not value_to_combos_dict.get(row_value):
-      value_to_combos_dict[row_value] = []
-    value_to_combos_dict[row_value].append(row)
+    word_values = [
+      determine_word_value(word) for word in row
+    ]
+    value_count = count_values_under_amount(3, word_values)
+    if value_count == 2 and row_value == 18:
+      sorted_row = sorted(row, key=cmp_to_key(compare_word_values))
+      print(word_values)
+      write_csv_file.write(','.join(sorted_row))
+      write_csv_file.write('\n')
+    
+
+    # if not value_to_combos_dict.get(row_value):
+    #   value_to_combos_dict[row_value] = []
+    # value_to_combos_dict[row_value].append(row)
   csv_file.close()
-  keys_list = [key for key in value_to_combos_dict]
-  keys_list.sort()
-  best_value = 100
-  if keys_list:
-    best_value = keys_list[0]
-  print('Best combos have a score of %s' % best_value)
-  best_combos = value_to_combos_dict[best_value]
-  best_combos.sort()
-  for best_combo in best_combos:
-    print(best_combo)
-  return value_to_combos_dict[best_value]
+  write_csv_file.close()
+  # keys_list = [key for key in value_to_combos_dict]
+  # keys_list.sort()
+  # best_value = 100
+  # if keys_list:
+  #   best_value = keys_list[0]
+  # print('Best combos have a score of %s' % best_value)
+  # best_combos = value_to_combos_dict[best_value]
+  # best_combos.sort()
+  # for best_combo in best_combos:
+  #   print(best_combo)
+  # return value_to_combos_dict[best_value]
 
 # For the record, the best combos of 4 letter words with a value of 40 are:
 # ['gawds', ' elfin', ' umpty', ' brock']
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from words import FILTERED_WORD_LIST
+from words import FILTERED_WORDS, UNIQUE_LETTER_WORDS
 from combos import print_best_combos
 from itertools import combinations
+import operator
+import csv
 
+# refix,umpty,wolds,ganch
 def check_for_working_combo(combo):
   combo_works = combination_has_unique_letters(combo)
   return combo_works
@@ -9,49 +12,110 @@ def check_for_working_combo(combo):
 
 def write_word_combos_csv(num_words):
   print('Getting combinations')
-  all_combinations = combinations(FILTERED_WORD_LIST, num_words)
+  all_combinations = combinations(
+    FILTERED_WORDS, 
+    num_words
+  )
   index = 0
-
-  interval = 1000000
+  interval = 10000000
+  csv_file = open("combos.csv", "a")
 
   for combination in all_combinations:
     index += 1
     if index % interval == 0:
       interval_num = index // interval
-      print('Processed %sM combos' % interval_num)
+      print('Processed %s x10M combos' % interval_num)
 
-    if not check_for_working_combo(combination):
+    if not combination_has_unique_letters(combination):
       continue
 
     combo_str = ','.join(combination)
     print('combination found: %s' % combo_str)
-    csv_file = open("combos.csv", "w")
     csv_file.write(combo_str)
     csv_file.write('\n')
-    csv_file.close()
 
-
-
+  csv_file.close()
 
 def combination_has_unique_letters(combination_tuple):
   letters = ''.join(combination_tuple)
   letters_list = list(letters)
   return len(letters_list) == len(set(letters_list))
 
-  
-def word_combo_contains_all_required_letters(word_combo):
-  REQUIRED_LETTERS = 'strnaeiou'
-  flattened_combo = ''.join(word_combo)
-  for letter in REQUIRED_LETTERS:
-    if letter not in flattened_combo:
+def get_sorted_letters(word1, word2):
+  combined_word = word1 + word2
+  return ''.join(sorted(combined_word))
+
+def get_remaining_letters(used_letters):
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  remaining_letters = ''
+  for letter in alphabet:
+    if letter not in used_letters:
+      remaining_letters += letter
+  return remaining_letters
+
+def sequence_contains_all_unused_letters(sequence, unused_letters):
+  for letter in sequence:
+    if letter not in unused_letters:
       return False
   return True
 
+def shares_no_letters(word1, word2):
+  for letter in word1:
+    if letter in word2:
+      return False
+  return True
+
+def shares_any_letters(word1, word2):
+  return not shares_no_letters(word1, word2)
+
+def four_sum_combos_to_csv():
+  word_list = UNIQUE_LETTER_WORDS
+  index_pairs = dict()
+  len_words = len(word_list)
+  csv_file = open("allcombos.csv", "a")
+
+  for i in range(0, len_words):
+    first_word = word_list[i]
+
+    compatible_words = [
+      word for word in word_list[i+1:] if shares_no_letters(first_word, word)
+    ]
+    print('%s/%s: %s has %s compatible words' % (i, len_words, first_word, len(compatible_words)))
+    for second_word in compatible_words:
+      word_combo = [first_word, second_word]
+      if not combination_has_unique_letters(word_combo):
+        continue
+
+      sorted_letters_key = get_sorted_letters(first_word, second_word)
+
+      compatible_keys = [key for key in index_pairs.keys() if shares_no_letters(key, sorted_letters_key)]
+      if compatible_keys:
+        print('Checking %s against %s keys' % (word_combo, len(compatible_keys)))
+
+
+      for key in compatible_keys:
+        pairs = index_pairs[key]
+        compatible_pairs = [
+          pair for pair in pairs if first_word not in pair and second_word not in pair
+        ]
+        print('Checking %s against %s pairs' % (word_combo, len(compatible_pairs)))
+
+        for pair in compatible_pairs:
+          combination = sorted(word_combo + pair) # Avoid duplicate
+          combo_str = ','.join(combination)
+          print('combination found: %s' % combo_str)
+          csv_file.write(combo_str)
+          csv_file.write('\n')
+
+      if index_pairs.get(sorted_letters_key): # create or append to list in key
+          index_pairs[sorted_letters_key].append(word_combo)
+      else:
+          index_pairs[sorted_letters_key] = [word_combo]
+
+
 def main():
-  combo_length = 4
-  write_word_combos_csv(combo_length)
-  print('------------')
-  # print_best_combos()
+  four_sum_combos_to_csv()
+
+
 
 main()
-
